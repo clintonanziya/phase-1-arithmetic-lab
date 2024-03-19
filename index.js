b0fcc3095337367c64e4635e6db9fe9ca3a59b52
clintonanziya <clintonanziya3@gmail.com>
2024-03-19 09:20:47 +0300

    completed-phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 14;
+let num4 = 5;
 
-// Write your code here
+let multiply = num1 * num2; // Result should be 62
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1; // Generates a random integer greater than 0
+
+let mod = num3 % num4; // Remainder of dividing num3 by num4 should be 4
+
+let max = Math.max(2, 4, 6, 8, 10, 20); // Finds the highest number in the set, value returned should be 20
